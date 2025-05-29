98d2b9e13daca663a34155e3e04bb201748d790e
Gsdagustavo <gugadanielalvez@gmail.com>
2025-05-28 22:14:45 -0300

    made the top app bar

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,58 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Hello World!')));
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: FloatingActionButton(
+                            onPressed: () {},
+                            shape: CircleBorder(),
+                            child: Icon(
+                              Icons.person,
+                              color: Theme.of(context).primaryColorLight,
+                            ),
+                          ),
+                        ),
+
+                        Row(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.visibility),
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.help_outline),
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.email),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
