b2b5e06c7923c5e9f320e3e9a5c995d0d2a059f6
Gsdagustavo <gugadanielalvez@gmail.com>
2025-05-29 17:25:22 -0300

    made the buttons for pix, paying, etc.

--- lib/main.dart
+++ lib/main.dart
@@ -39,50 +39,126 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             const TopAppBar(),
-        
+            const BalanceWidget(),
+
             Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: InkWell(
-                child: Ink(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 10),
-                            child: Text(
-                              'Conta',
-                              style: TextStyle(
-                                fontSize: 22,
-                                fontWeight: FontWeight.bold,
-                              ),
-        
-                              textAlign: TextAlign.start,
-                            ),
-                          ),
-        
-                          Text(
-                            'R\$ ${balance.toStringAsFixed(2)}',
-                            style: TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                            ),
-        
-                            textAlign: TextAlign.start,
-                          ),
-                        ],
-                      ),
-        
-                      Icon(Icons.keyboard_arrow_right),
-                    ],
-                  ),
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+
+                    /// Pix button
+                    ActionButton(iconData: Icons.pix, label: 'Pix'),
+
+                    /// Pay button
+                    ActionButton(iconData: Icons.payment, label: 'Pagar'),
+
+                    /// Loan button
+                    ActionButton(
+                      iconData: Icons.payment,
+                      label: 'Pegar empréstimo',
+                    ),
+                  ],
                 ),
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ActionButton extends StatelessWidget {
+  const ActionButton({super.key, required this.iconData, required this.label});
+
+  final IconData iconData;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 15),
+      child: SizedBox(
+        width: 90,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 15),
+              child: InkWell(
+                onTap: () {},
+                customBorder: CircleBorder(),
+
+                child: Ink(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    shape: BoxShape.circle,
+                  ),
+
+                  padding: const EdgeInsets.all(20),
+
+                  child: Icon(iconData, size: 30),
+                ),
+              ),
+            ),
+
+            Text(
+              label,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BalanceWidget extends StatelessWidget {
+  const BalanceWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: InkWell(
+        child: Ink(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: Text(
+                      'Conta',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+
+                  Text(
+                    'R\$ ${balance.toStringAsFixed(2)}',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+
+                    textAlign: TextAlign.start,
+                  ),
+                ],
+              ),
+
+              Icon(Icons.keyboard_arrow_right),
+            ],
+          ),
         ),
       ),
     );
